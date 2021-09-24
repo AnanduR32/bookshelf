6f988c40f833a2b8a6ad370500d6b496eeb7cf9b
Anandu R <ananduraj32@gmail.com>
2021-09-24 14:34:43 +0530

    first commit

---
+++ BookshelfAPIs/BookshelfAPIs/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookshelfAPIs.Models;
+using System.Web.Http.Cors;
+
+namespace BookshelfAPIs.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class BooksController : ApiController
+    {
+        BooksDatabase db = BooksDatabase.instantiateDB();
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            return db.Books;
+        }
+        public Book GetBooks(string isbn)
+        {
+            return db.Books.FirstOrDefault(book => book.ISBN == isbn);
+        }
+        
+        [HttpPost]
+        public string PostBook(Book book)
+        {
+            db.PostData(book);
+            return String.Format("Successfully added book! Author: {0} Name: {1} ISBN: {2} Date: {3}", book.Author, book.Name, book.ISBN, book.Date);
+        }
+        [HttpDelete]
+        public string DeleteBook(Book book)
+        {
+            db.DeleteData(book);
+            return String.Format("Deleted book where ISBN == {0}", book.ISBN);
+        }
+    }
+}
---
+++ BookshelfAPIs/BookshelfAPIs/Models/BooksDatabase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Globalization;
+using System.Data.SqlClient;
+
+namespace BookshelfAPIs.Models
+{
+    public class BooksDatabase
+    {
+
+        private string connectionString = @"Data Source=localhost\SQLEXPRESS01; database=BookshelfDB; Trusted_Connection=True";
+        public List<Book> Books = new List<Book>();
+        private static BooksDatabase Instance = null;
+        public static BooksDatabase instantiateDB()
+        {
+            if (Instance == null)
+            {
+                Instance = new BooksDatabase();
+            }
+            return Instance;
+        }
+        private BooksDatabase()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlCommand cmd = con.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM books_collection";
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                Books = ConvertToBookList(dr);
+            }
+        }
+
+        public void PostData(Book book)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlCommand cmd = con.CreateCommand())
+            {
+                cmd.CommandText = String.Format("insert into books_collection (Author, Name, ISBN, Date) values ('{0}', '{1}', '{2}', '{3}')",book.Author, book.Name, book.ISBN, book.Date);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            Books.Add(book);
+        }
+        public void DeleteData(Book Querybook)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlCommand cmd = con.CreateCommand())
+            {
+                cmd.CommandText = String.Format("DELETE FROM books_collection WHERE ISBN='{0}'", Querybook.ISBN);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            Books.Remove(Books.FirstOrDefault(book => book.ISBN == Querybook.ISBN));
+        }
+
+        private List<Book> ConvertToBookList(SqlDataReader reader)
+        {
+            var rows = new List<Book>();
+            while (reader.Read())
+            {
+                // rows.Add(columns.ToDictionary(column => column, column => reader[column]));
+                rows.Add(new Book()
+                {
+                    Author = reader["Author"].ToString(),
+                    Name = reader["Name"].ToString(),
+                    ISBN = reader["ISBN"].ToString(),
+                    Date = reader["Date"].ToString(),
+                });
+            }
+            return rows;
+        }
+    }
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Date { get; set; }
+    }
+}
